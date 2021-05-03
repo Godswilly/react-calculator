1be6fdc09300c734596977ce275d3fe2728b2cc5
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-03 12:18:36 +0100

    App render Display and ButtonPanel components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <ButtonPanel />
+    <Display />
+  </div>
+);
 
 export default App;
