8e332bdc2bdaaefbe1595f69bb72d6f1ecf1b210
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-05 12:54:03 +0100

    Implement handleClick functionality

--- src/components/App.js
+++ src/components/App.js
@@ -11,15 +11,30 @@ class App extends React.Component {
       total: null,
       next: null,
       operation: null,
-    };
+    }; this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (buttonName) => {
+    const {
+      total, next, operation,
+    } = this.state;
+    const result = calculate({
+      total, next, operation,
+    }, buttonName);
+    this.setState({
+      total: result.total,
+      next: result.next,
+      operation: result.operation,
+    });
   }
 
   render() {
+    const { total, next } = this.state;
     return (
-      <div>
-        <Display />
-        <ButtonPanel />
-      </div>
+      <di>
+        <Display result={total} next={next} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </di>
     );
   }
 }
