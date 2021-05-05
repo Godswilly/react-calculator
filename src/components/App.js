384a82e19c6a7fd86841cdaf3db2197c698407b9
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-05 12:40:41 +0100

    Initialize state in the constructor

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div>
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 }
 
 export default App;
